944bd69b0f5c5fe5a1ce53525110d00ba36615e8
Camilo Henrique <camilo.henri.ch@gmail.com>
2024-12-01 14:52:03 -0300

    inicial

---
+++ src/main/java/br/com/chenrique/secury_jwt_estudo/SecuryJwtEstudoApplication.java
@@ -0,0 +1,13 @@
+package br.com.chenrique.secury_jwt_estudo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecuryJwtEstudoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecuryJwtEstudoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/chenrique/secury_jwt_estudo/SecuryJwtEstudoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.chenrique.secury_jwt_estudo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecuryJwtEstudoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
